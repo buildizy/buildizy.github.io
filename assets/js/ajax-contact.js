ff5279f2fe323d4653e71d12e1112df707245b7a
SiDevesh <sid.swapnildevesh@gmail.com>
2019-11-07 18:46:36 +0530

    Fix form on empty submission

--- assets/js/ajax-contact.js
+++ assets/js/ajax-contact.js
@@ -11,38 +11,44 @@ $(function() {
 		// Stop the browser from submitting the form.
 		e.preventDefault();
 
-		// Serialize the form data.
-		var formData = $(form).serialize();
+		setTimeout(function() {
+			if (
+				[...$('.form-group')]
+					.filter(function(formGroup) {
+						return formGroup.classList.contains('has-error') ||
+						formGroup.classList.contains('has-danger');
+					}).length === 0
+			) {
+				// Serialize the form data.
+				var formData = $(form).serialize();
 
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
+				// Submit the form using AJAX.
+				$.ajax({
+					type: 'POST',
+					url: $(form).attr('action'),
+					data: formData
+				})
+				.done(function() {
+					// Make sure that the formMessages div has the 'success' class.
+					$(formMessages).removeClass('error');
+					$(formMessages).addClass('success');
 
-			// Set the message text.
-			$(formMessages).text('Thanks! We will get in touch soon.');
+					// Set the message text.
+					$(formMessages).text('Thanks! We will get in touch soon.');
 
-			// Clear the form.
-			$('#contact-form input,#contact-form textarea').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
+					// Clear the form.
+					$('#contact-form input,#contact-form textarea').val('');
+				})
+				.fail(function() {
+					// Make sure that the formMessages div has the 'error' class.
+					$(formMessages).removeClass('success');
+					$(formMessages).addClass('error');
 
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+					// Set the message text.
+					$(formMessages).text('Oops! An error occured and your message could not be sent.');
+				});
 			}
-		});
+		}, 100);
 	});
 
 });
